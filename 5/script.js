84944bd3a8788226b3cd6f8fdc14a19b0f3cc625
shima9397 <shima.nejati.72@gmail.com>
2021-09-02 17:16:23 +0430

    task-5 added

---
+++ 5/script.js
@@ -0,0 +1,8 @@
+//5- Create a function to accept a string and return the size of it.
+
+let getSize = (myString) => {
+  return myString.length;
+};
+
+console.log(`myString size is: ${getSize("shima")}`);
+console.log(`myString size is: ${getSize("if you never try you never know")}`);
